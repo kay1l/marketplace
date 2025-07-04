41a50aadbd83a2c593642521e635af22c4e9958c
kay1l <gomezkyle3102@gmail.com>
2025-07-04 11:35:45 +0800

    last

--- src/app/pages/create/page.tsx
+++ src/app/pages/create/page.tsx
@@ -3,6 +3,7 @@
 import CreateListingCard from "@/custom_components/create_listing_cards";
 import Header from "@/custom_components/layouts/header";
 import Sidebar from "@/custom_components/layouts/sidebar";
+import { Suspense } from "react";
 
 export default function MarketplaceLayout() {
   return (
@@ -11,7 +12,9 @@ export default function MarketplaceLayout() {
       <div className="flex">
         <Sidebar />
         <main className="p-4 w-full sm:ml-64">
-          <CreateListingCard />
+          <Suspense fallback={<div>Loading...</div>}>
+            <CreateListingCard />
+          </Suspense>
         </main>
       </div>
     </div>
